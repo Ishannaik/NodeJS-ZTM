0c6726f0f26329535ac8d79adffaf83b370b0243
Ishan <ishannaik7@gmail.com>
2024-02-16 17:15:05 +0530

    basic changes

---
+++ PACKAGE-EXAMPLE/request.js
@@ -0,0 +1,12 @@
+const axios = require('axios')
+
+axios.get('https://www.google.com')
+    .then((response)=>{
+        console.log(response);
+    })  
+    .catch((err) => {
+        console.log(err);
+    })
+    .then(()=>{
+    console.log('All Done')
+     });
