08b6a1642c2783bc861a225ef4f540350f77e41a
Ishan <ishannaik7@gmail.com>
2024-02-16 16:48:30 +0530

    index.js added

--- MODULES-EXAMPLE/request.js
+++ MODULES-EXAMPLE/request.js
@@ -1,3 +1,4 @@
+const REQUEST_TIMEOUT = 500;
 function encrypt(data){
     return 'encrypted data'
 }
@@ -9,5 +10,6 @@ function send(url,data){
 
 
 module.exports = {
+    REQUEST_TIMEOUT,
     send,
 };
