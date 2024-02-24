36265a05f1443647b34d230378d8408669404f7e
Ishan <ishannaik7@gmail.com>
2024-02-24 09:17:29 +0530

    FIXED AGAIN

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -14,5 +14,7 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 module.exports = app;
