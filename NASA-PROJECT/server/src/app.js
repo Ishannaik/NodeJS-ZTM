1e140e0b7b7541228a16ab4934e7a1b41b082b91
Ishan <ishannaik7@gmail.com>
2024-02-24 08:50:36 +0530

    Waste of time

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 path = require("path");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
-
+const morgan = require("morgan");
 const app = express();
 
 app.use(
@@ -10,6 +10,7 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
