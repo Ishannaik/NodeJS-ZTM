13a14e8394225a1d5fade2cf76fdd03640a72169
Ishan <ishannaik7@gmail.com>
2024-02-24 09:54:39 +0530

    FINALLY FIXED!!!!

--- NASA-PROJECT/server/src/app.js
+++ NASA-PROJECT/server/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
-path = require("path");
+const path = require("path");
 const cors = require("cors");
-const planetsRouter = require("./routes/planets/planets.router");
 const morgan = require("morgan");
+
+const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
+
 const app = express();
 
 app.use(
@@ -11,9 +14,13 @@ app.use(
   })
 );
 app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.use(launchesRouter);
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
