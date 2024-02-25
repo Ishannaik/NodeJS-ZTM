a88ed3f91d8b932fde0035734964a3e60d884399
Ishan <ishannaik7@gmail.com>
2024-02-25 14:50:52 +0530

    I HATE NODEJS

--- NASA-PROJECT/server/src/routes/launches/launches.controller.js
+++ NASA-PROJECT/server/src/routes/launches/launches.controller.js
@@ -4,6 +4,14 @@ function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+  const launch = req.body;
+  launch.launchDate = new Date(launch.launchDate);
+  addNewLaunch(launch);
+  return res.status(201).json(launch);
+}
+
 module.exports = {
   httpGetAllLaunches,
+  httpAddNewLaunch,
 };
