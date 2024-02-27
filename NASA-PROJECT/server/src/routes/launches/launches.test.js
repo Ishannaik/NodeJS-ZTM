ff425855ef4a7a14eb05c7d15d2352339d019c82
Ishan <ishannaik7@gmail.com>
2024-02-27 07:13:36 +0530

    Updated launches.test.js

---
+++ NASA-PROJECT/server/src/routes/launches/launches.test.js
@@ -0,0 +1,12 @@
+describe("Test GET /launches", () => {
+  test("It should respond with 200 success", async () => {
+    const response = await request(app).get("/launches");
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("Test POST /launch", () => {
+  test("It should respond with 200 success", () => {});
+  test("It should catch missing required properties", () => {});
+  test("It should catch invalid dates", () => {});
+});
