fa77d93bd6252c436cd0f538582a8e2114a12a27
Ishan <ishannaik7@gmail.com>
2024-02-28 17:58:13 +0530

    Added Clustering to the server.js file to improve performance

--- PERFORMANCE-EXAMPLE/server.js
+++ PERFORMANCE-EXAMPLE/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
 
 const app = express();
 
@@ -12,14 +14,27 @@ function delay(duration) {
 app.get("/", (req, res) => {
   //   json.stringify({ name: "John", age: 30 });
   //   json.parse('{"name":"John","age":30}');
-  res.send("Performance example");
+  res.send(`Performance example: ${process.pid}`);
 });
 
 app.get("/timer", (req, res) => {
   delay(9000);
-  res.send("Timer example");
+  res.send(`Timer example ${process.pid}`);
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+console.log("Running server.js now...");
+
+if (cluster.isMaster) {
+  console.log("Master has been started...");
+  const NUM_WORKERS = os.cpus().length;
+  console.log(NUM_WORKERS, "CPUs available.");
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log("Worker process started.");
+
+  app.listen(3000, () => {
+    console.log("Server is running on port http://localhost:3000");
+  });
+}
