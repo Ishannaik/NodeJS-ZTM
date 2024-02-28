dcead86fb15886e4c0a60de396761c54064254da
Ishan <ishannaik7@gmail.com>
2024-02-28 18:17:12 +0530

    Changed the delay to 4000ms in server.js and added pm2 to the package.json file and globally installed it. Also added a logs.txt file to the project.

--- PERFORMANCE-EXAMPLE/server.js
+++ PERFORMANCE-EXAMPLE/server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const cluster = require("cluster");
-const os = require("os");
 
 const app = express();
 
@@ -13,28 +11,18 @@ function delay(duration) {
 
 app.get("/", (req, res) => {
   //   json.stringify({ name: "John", age: 30 });
-  //   json.parse('{"name":"John","age":30}');
+  //   json.parse('{"name":"ohn","age":30}');
   res.send(`Performance example: ${process.pid}`);
 });
 
 app.get("/timer", (req, res) => {
-  delay(9000);
+  delay(4000);
   res.send(`Timer example ${process.pid}`);
 });
 
 console.log("Running server.js now...");
 
-if (cluster.isMaster) {
-  console.log("Master has been started...");
-  const NUM_WORKERS = os.cpus().length;
-  console.log(NUM_WORKERS, "CPUs available.");
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log("Worker process started.");
-
-  app.listen(3000, () => {
-    console.log("Server is running on port http://localhost:3000");
-  });
-}
+console.log("Worker process started.");
+app.listen(3000, () => {
+  console.log("Server is running on port http://localhost:3000");
+});
