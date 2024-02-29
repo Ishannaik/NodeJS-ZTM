e0300ece2311714b6bea5be5d7fc3b54802579cd
Ishan <ishannaik7@gmail.com>
2024-02-29 19:42:58 +0530

    Added MongoDB and connected it to the server.js file and added the necessary dependencies to the package.json file.

--- NASA-PROJECT/server/src/server.js
+++ NASA-PROJECT/server/src/server.js
@@ -1,11 +1,27 @@
 const http = require("http");
+
+const mongoose = require("mongoose");
 const app = require("./app");
-const PORT = process.env.PORT || 8000;
 const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
+const PORT = process.env.PORT || 8000;
+
+mongoose.connection.once("connected", () => {
+  console.log("Mongoose connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error("Mongoose connection error:", err);
+});
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:ce4AHw70N45hZDRD@nasacluster.92kmbsl.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
