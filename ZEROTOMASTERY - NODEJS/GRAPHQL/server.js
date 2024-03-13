18224d52a1561157652817e158a58a8b203218ee
Ishan <ishannaik7@gmail.com>
2024-03-13 08:32:31 +0530

    Designing an E-Commerce Schema with GraphQL

--- ZEROTOMASTERY - NODEJS/GRAPHQL/server.js
+++ ZEROTOMASTERY - NODEJS/GRAPHQL/server.js
@@ -4,13 +4,66 @@ const { buildSchema } = require("graphql");
 
 const schema = buildSchema(`
 type Query {
-    description: String
+    products: [Product] 
+    orders: [Order]
+}
+type Product {
+  id: ID!
+    description: String!
+    reviews: [Review]
     price: Float
+}
+type Review {
+    rating: Int!
+    comment: String!
+}
+type Order{
+  date: String!
+  subtotal: Float!
+  items: [OrderItem]
+}
+type OrderItem{
+  product: Product!
+  quantity: Int!
 }
 `);
 const root = {
-  description: "Red Shoes",
-  price: 123.45,
+  products: [
+    {
+      id: "redshoe",
+      description: "A red shoe",
+      price: 10.0,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jeans",
+      price: 50.0,
+    },
+  ],
+  orders: [
+    {
+      date: "2021-01-01",
+      subtotal: 60.0,
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: "A red shoe",
+            price: 10.0,
+          },
+          quantity: 2,
+        },
+        {
+          product: {
+            id: "bluejean",
+            description: "Blue Jeans",
+            price: 50.0,
+          },
+          quantity: 1,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
