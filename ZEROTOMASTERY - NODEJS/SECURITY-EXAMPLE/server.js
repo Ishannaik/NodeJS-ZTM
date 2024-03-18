4bc00c68945c92a06beb66e95acdc0d89c53e8e6
Ishan <ishannaik7@gmail.com>
2024-03-18 09:39:01 +0000

    Added helmet middleware to the project to secure the application

--- ZEROTOMASTERY - NODEJS/SECURITY-EXAMPLE/server.js
+++ ZEROTOMASTERY - NODEJS/SECURITY-EXAMPLE/server.js
@@ -2,10 +2,13 @@ const fs = require("fs");
 const https = require("http");
 const path = require("path");
 const express = require("express");
+const helmet = require("helmet");
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get("/secret", (req, res) => {
   return res.send("This is a secret");
@@ -15,7 +18,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-https.createServer(
+https
+  .createServer(
     {
       key: fs.readFileSync("key.pem"),
       cert: fs.readFileSync("cert.pem"),
