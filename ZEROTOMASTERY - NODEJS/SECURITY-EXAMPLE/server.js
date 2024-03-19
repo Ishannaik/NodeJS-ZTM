1a8d5273d966223962268d635157c0e766611a2b
Ishan <ishannaik7@gmail.com>
2024-03-19 02:18:21 +0000

    Added google OAUTH callbacks A#	modified:   ZEROTOMASTERY - NODEJS/SECURITY-EXAMPLE/server.js

--- ZEROTOMASTERY - NODEJS/SECURITY-EXAMPLE/server.js
+++ ZEROTOMASTERY - NODEJS/SECURITY-EXAMPLE/server.js
@@ -4,6 +4,9 @@ const path = require("path");
 const express = require("express");
 const helmet = require("helmet");
 require("dotenv").config();
+const passport = require("passport-google-oauth20");
+const { Stratergy } = require("passport-google-oauth20");
+
 const port = process.env.PORT || 3000;
 
 const config = {
@@ -11,9 +14,24 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+passport.use(
+  new Stratergy({
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    callbackURL: "/auth/google/callback",
+  })
+),
+  function verifyCallback(accessToken, refreshToken, profile, done) {
+    console.log("accessToken", accessToken);
+    console.log("refreshToken", refreshToken);
+    console.log("profile", profile);
+    done(null, profile);
+  };
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; //TODO
